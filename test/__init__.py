c76fd3354b2368f6fabba96a0bc5491624ac1c39
DheerajKumarNomula <107257072+DheerajKumarNomula@users.noreply.github.com>
2022-12-12 23:30:30 +0530

    temp

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,7 @@
 import logging
-
+import csv
 import azure.functions as func
-
+import temp
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -15,10 +15,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
+    print("Dheeraj")
+    server = "foxie-poc-ondemand.sql.azuresynapse.net"
+    database = "test_db3"
+    username = "sqladminuser"
+    password = "increff@ADMIN123"
+    sql = "select * from valid_stores;"
+    local_file = "output.csv"
+    temp.execute_sql(server, database, username, password, sql, local_file)
+    with open(local_file, 'rb') as f:
+            return func.HttpResponse(
+            body=f.read(),
+            mimetype='text/csv',
+            status_code=200
         )
+
